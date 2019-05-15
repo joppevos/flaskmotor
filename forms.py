b34db438a09d3eeb6e8599d85ca1fb2c0be0fcaa
joppe <joppe.vos@gmail.com>
2019-05-15 09:37:30 +0200

    added database

--- forms.py
+++ forms.py
@@ -1,16 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, ValidationError, InputRequired
+
+
+def validate_digit(form, field):
+    try:
+        val = int(field.data)
+    except ValueError:
+        message = field.gettext("Please enter a number")
+        # field.errors.append("Please enter a number")
+        raise ValidationError("Please enter a number")
 
 
 class LoginForm(FlaskForm):
-    age    = StringField('age', validators=[DataRequired()])
-    amount    = StringField('amount', validators=[DataRequired()])
-    dick    = StringField('dick', validators=[DataRequired()])
+    age    = IntegerField('age', validators=[InputRequired(), validate_digit])
+    amount    = IntegerField('amount', validators=[InputRequired(), validate_digit])
+    dick    = IntegerField('dick', validators=[InputRequired(), validate_digit])
     submit = SubmitField('calculate')
 
-    def validate_name(self, form):
-        try:
-            val = int(form)
-        except ValueError:
-            ValidationError("That's not an int!")
+
--- routes.py
+++ routes.py
@@ -1,28 +1,62 @@
-
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import LoginForm
+from forms import LoginForm, validate_digit
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '53b95a20e6ea0bb89ec81dd80b54190d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-motoren = ['hayabusa', 'bmw1200gs']
+class User(db.Model):
+    name = db.Column(db.String(50), primary_key=True)
+    age = db.Column(db.String(50), nullable=False)
+    budget = db.Column(db.String(50), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.String(300), unique=True, nullable=True)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.text}'"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = LoginForm()
     if form.validate_on_submit():
-        choice = get_motorcycle(int(form.dick.data), int(form.age.data))
-        return render_template('motor_choice.html', form=form, motor=motoren[choice])
-    return render_template('login.html', form=form)
+        motor = get_motorcycle(dick=form.dick.data, age=form.age.data, budget=form.amount.data)
+        return render_template('login.html', form=form, answer=True, motor=motor)
+    return render_template('login.html', form=form, answer=False)
 
 
-def get_motorcycle(dick, age):
-    if age > 40:
-        return 1
-    else:
-        return 0
+def get_motorcycle(dick, age, budget):
+    if dick <= 15:
+        if age >= 35:
+            if budget >= 10000:
+                'small_old_rich'
+                return 'bmwgs'
+            else:
+                'small_old_poor'
+                return 'hondashadow'
+
+        elif age < 35:
+            if budget >= 10000:
+                return 'triumph'
+            else:
+                return 'hayabusa'
+
+    elif dick > 15:
+        if age >= 35:
+            if budget >= 10000:
+                return 'honda goldwing'
+            else:
+                return 'tjhis'
+
+        else:
+            if budget >= 10000:
+                return 'wr450r'
+            else:
+                return 'honda vtr'
 
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
